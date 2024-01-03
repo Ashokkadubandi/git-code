a306657ad98a92d296b36b8f01b8dbf27b067bc5
Ashok <velmaashok765@gmail.com>
2024-01-03 19:43:41 +0000

    API data is added as books

--- src/Components/Books/index.js
+++ src/Components/Books/index.js
@@ -1,10 +1,28 @@
 import { Component } from "react";
 import Header from "../Header";
+import "./index.css";
 
 class Books extends Component {
+  state = { booksData: [] };
+
   componentDidMount() {
     this.getBooksApi();
   }
+
+  renderBooksData = (data) => {
+    console.log(data);
+    const { title, price, subtitle, image, isbn13 } = data;
+    return (
+      <li className="book-list">
+        <img src={image} alt="img-gen" className="image" />
+        <p className="title">{title}</p>
+        <p className="price">{price}</p>
+        <button type="button" className="add-cart-btn">
+          Add to cart
+        </button>
+      </li>
+    );
+  };
 
   getBooksApi = async () => {
     const url = "https://api.itbook.store/1.0/search/mongodb";
@@ -13,14 +31,18 @@ class Books extends Component {
     };
     const response = await fetch(url, options);
     const data = await response.json();
-    console.log(data);
+    this.setState({ booksData: data.books });
   };
   render() {
+    const { booksData } = this.state;
+    console.log(booksData);
     return (
       <div>
         <Header />
         <p>Get books API</p>
-        <p>Books will be displayed here...</p>
+        <ul className="books-container">
+          {booksData.map((each) => this.renderBooksData(each))}
+        </ul>
       </div>
     );
   }
