9ce72164cfa755b7a7146858d223f9df3ef7309f
Ashok <velmaashok765@gmail.com>
2024-01-07 08:31:53 +0000

    Solution

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -7,7 +7,9 @@ const Home = () => (
     <Header />
     <div className="home">
       <div className="card-box">
-        <p className="heading-books">Books that you want know</p>
+        <p className="heading-books">
+          Books that you want know Deployed by Vercel
+        </p>
         <p className="desc">
           Explore your shopping here with relevant things that you would might
           buy such products
